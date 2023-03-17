cebd62712c02e8a2c0265345b2041c03e3c53793
lenaruppert <l.zi@gmx.de>
2023-03-17 16:57:01 +0100

    add @Document annotation in backend record Client and make id optional in frontend model

--- backend/src/main/java/com/github/lenaruppert/backend/model/Client.java
+++ backend/src/main/java/com/github/lenaruppert/backend/model/Client.java
@@ -1,7 +1,9 @@
 package com.github.lenaruppert.backend.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public record Client(
         @Id
         String id,
--- frontend/src/model/Client.ts
+++ frontend/src/model/Client.ts
@@ -1,4 +1,4 @@
 export type Client = {
-    id: string,
+    id?: string,
     name: string
 }
