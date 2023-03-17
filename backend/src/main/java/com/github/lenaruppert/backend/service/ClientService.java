c12005adc98683a90a09a3d564183f676057b517
lenaruppert <l.zi@gmx.de>
2023-03-17 12:41:33 +0100

    create method addClient

--- backend/src/main/java/com/github/lenaruppert/backend/service/ClientService.java
+++ backend/src/main/java/com/github/lenaruppert/backend/service/ClientService.java
@@ -1,5 +1,6 @@
 package com.github.lenaruppert.backend.service;
 
+import com.github.lenaruppert.backend.model.Client;
 import com.github.lenaruppert.backend.repository.ClientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,12 @@ public class ClientService {
     private final ClientRepository clientRepository;
 
     private final IdService idService;
+
+    public Client addClient(Client client) {
+        Client clientToAdd = new Client(
+                idService.generateId(),
+                client.name()
+        );
+        return clientRepository.save(clientToAdd);
+    }
 }
