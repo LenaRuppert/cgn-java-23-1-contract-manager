c5799e4c1743080b52a0c675e1d61ec3fa195eec
lenaruppert <l.zi@gmx.de>
2023-03-17 15:41:50 +0100

    add hook useClient with function addClient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import {useClients} from "./hooks/useClients";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const [clients, addClient] = useClients()
+
+  return (
+      <div className="App">
+
+      </div>
   );
 }
 
---
+++ frontend/src/hooks/useClients.ts
@@ -0,0 +1,16 @@
+import {useState} from "react";
+import {Client} from "../model/Client";
+import axios from "axios";
+
+export function useClients() {
+
+    const [clients, setClients] = useState<Client[]>([])
+
+    function addClient(client: Client) {
+        return axios.post("api/clients/add", client)
+            .then(response => response.data)
+            .then(data => setClients(prevState => [...prevState, data]))
+    }
+
+    return [clients, addClient]
+}
