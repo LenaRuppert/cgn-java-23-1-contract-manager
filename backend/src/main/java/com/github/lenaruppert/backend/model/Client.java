b4c10875f359f94704811db30066db1adf3b044b
lenaruppert <l.zi@gmx.de>
2023-03-25 09:36:40 +0100

    add jobList to Client model

--- backend/src/main/java/com/github/lenaruppert/backend/model/Client.java
+++ backend/src/main/java/com/github/lenaruppert/backend/model/Client.java
@@ -3,9 +3,12 @@ package com.github.lenaruppert.backend.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document
 public record Client(
         @Id
         String id,
-        String name) {
+        String name,
+        List<Job> jobList) {
 }
