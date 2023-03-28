1241da93b19562c292af467d6aec8f8453f9bd35
lenaruppert <l.zi@gmx.de>
2023-03-28 14:47:06 +0200

    create component ClientJobs

---
+++ frontend/src/components/ClientJobs.tsx
@@ -0,0 +1,39 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Job} from "../model/Job";
+import axios from "axios";
+import JobCard from "./JobCard";
+import {Grid} from "@mui/material";
+
+export default function ClientJobs() {
+    const params = useParams()
+    const id: string | undefined = params.id
+
+    const [jobsClient, setJobsClient] = useState<Job[] | undefined>()
+
+    const requestURL: string = "/api/clients/" + {id} + "getJobs"
+
+    useEffect(() => {
+        axios.get(requestURL)
+            .then(response => {
+                setJobsClient(response.data)
+            })
+            .catch(error => console.error(error))
+    }, [requestURL])
+
+    const jobCards = jobsClient?.map(job => {
+        return (
+            <JobCard job={job} key={job.id}/>
+        )
+    })
+
+    return (
+        (
+            <Grid sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Grid container item xs={10} justifyContent='center' marginTop={5}>
+                    {jobCards}
+                </Grid>
+            </Grid>
+        )
+    )
+}
