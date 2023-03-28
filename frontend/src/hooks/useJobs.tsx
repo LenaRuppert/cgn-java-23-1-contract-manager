eb528a32a29a293fc62842c0021232294982deff
lenaruppert <l.zi@gmx.de>
2023-03-28 11:29:36 +0200

    add getAllJobs to frontend

--- frontend/src/hooks/useJobs.tsx
+++ frontend/src/hooks/useJobs.tsx
@@ -6,6 +6,14 @@ export function useJobs() {
 
     const [jobs, setJobs] = useState<Job[]>([])
 
+    function getAllJobs() {
+        axios.get("/api/jobs/all")
+            .then(response => {
+                setJobs(response.data)
+            })
+            .catch(console.error)
+    }
+
     function addJob(id: string | undefined, job: Job) {
         return axios.post("/api/clients/" + id + "/addJob", job)
             .then(response => response.data)
