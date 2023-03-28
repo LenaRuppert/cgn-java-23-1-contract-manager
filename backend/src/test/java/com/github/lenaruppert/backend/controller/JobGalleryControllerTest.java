6469e980903ce59adc5d4ed9d3a94e81338ab951
lenaruppert <l.zi@gmx.de>
2023-03-28 11:10:10 +0200

    create tests controller

---
+++ backend/src/test/java/com/github/lenaruppert/backend/controller/JobGalleryControllerTest.java
@@ -0,0 +1,55 @@
+package com.github.lenaruppert.backend.controller;
+
+import com.github.lenaruppert.backend.model.Job;
+import com.github.lenaruppert.backend.repository.JobRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class JobGalleryControllerTest {
+
+    @Autowired
+    JobRepository jobRepository;
+
+    @Autowired
+    MockMvc mockMvc;
+    Job jobOne;
+
+    @BeforeEach
+    void setUp() {
+        jobOne = new Job("1", "titleOfJob", "1");
+    }
+
+    @Test
+    @DirtiesContext
+    void whenListAllJobsAndJobListIsEmpty_thenReturnEmptyList() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/jobs/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void whenListAllJobsWithOneJobInDataBase_thenReturnListWithOneJob() throws Exception {
+        jobRepository.save(jobOne);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/jobs/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        [
+                        {"id": "1",
+                        "title":  "titleOfJob",
+                        "clientId": "1"}
+                        ]
+                        """));
+    }
+}
