23afca604db0a5064691b4f454a5d703633bd1f7
lenaruppert <l.zi@gmx.de>
2023-04-02 20:23:03 +0200

    adjust test controller

--- backend/src/test/java/com/github/lenaruppert/backend/controller/MongoUserControllerTest.java
+++ backend/src/test/java/com/github/lenaruppert/backend/controller/MongoUserControllerTest.java
@@ -1,6 +1,5 @@
 package com.github.lenaruppert.backend.controller;
 
-import com.github.lenaruppert.backend.model.MongoUser;
 import com.github.lenaruppert.backend.repository.MongoUserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -24,19 +22,6 @@ class MongoUserControllerTest {
 
     @Autowired
     MongoUserRepository mongoUserRepository;
-
-    @Test
-    @DirtiesContext
-    @WithMockUser(username = "user", password = "password")
-    void getMe_whenAuthenticated_thenUsername() throws Exception {
-        mongoUserRepository.save(new MongoUser("1", "user", "password", "BASIC"));
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/me")
-                        .with(csrf()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.username").value("user"))
-                .andExpect(jsonPath("$.password").isEmpty());
-    }
-
     @Test
     @DirtiesContext
     void create_whenValid_then200() throws Exception {
