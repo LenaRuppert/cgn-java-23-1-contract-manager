49020d048940138413d7fcabedb2bd96707f84ae
lenaruppert <l.zi@gmx.de>
2023-04-03 16:17:01 +0200

    adjust jobController tests

--- backend/src/test/java/com/github/lenaruppert/backend/controller/JobControllerTest.java
+++ backend/src/test/java/com/github/lenaruppert/backend/controller/JobControllerTest.java
@@ -1,20 +1,24 @@
 package com.github.lenaruppert.backend.controller;
 
+import com.github.lenaruppert.backend.model.Client;
 import com.github.lenaruppert.backend.model.Job;
+import com.github.lenaruppert.backend.repository.ClientRepository;
 import com.github.lenaruppert.backend.repository.JobRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -24,12 +28,20 @@ class JobControllerTest {
     JobRepository jobRepository;
 
     @Autowired
+    ClientRepository clientRepository;
+
+    @Autowired
     MockMvc mockMvc;
     Job jobOne;
+    Client clientOne;
 
     @BeforeEach
     void setUp() {
+
         jobOne = new Job("1", "titleOfJob", "description", "street", "1a", "11111", "city", "1");
+        clientOne = new Client("1", "nameOfClient", new ArrayList<>());
+        clientRepository.save(clientOne);
+        jobRepository.save(jobOne);
     }
 
     @Test
@@ -78,6 +90,61 @@ class JobControllerTest {
                         "city": "city",
                         "clientId": "1"
                         }
+                        """));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "user", password = "password")
+    void whenAddJob_thenReturnNewJob() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/jobs/add/1").with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                "title": "titleOfJob",
+                                "description": "description",
+                                "street": "street",
+                                "houseNumber": "1",
+                                "postalCode": "11111",
+                                "city": "city",
+                                "clientId": "1"
+                                }
+                                """))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                                """
+                                                {
+                                                "title": "titleOfJob",
+                                        "description": "description",
+                                        "street": "street",
+                                        "houseNumber": "1",
+                                        "postalCode": "11111",
+                                        "city": "city",
+                                        "clientId": "1"
+                                                }
+                                                """
+                        )
+                )
+                .andExpect(jsonPath("$.id").isNotEmpty());
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "user", password = "password")
+    void whenGetJobsByClientId_thenReturnListOfJobs() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/jobs/get/1").with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        [
+                        {"id": "1",
+                        "title": "titleOfJob",
+                        "description": "description",
+                        "street": "street",
+                        "houseNumber": "1a",
+                        "postalCode": "11111",
+                        "city": "city",
+                        "clientId": "1"}
+                        ]
                         """));
     }
 }
