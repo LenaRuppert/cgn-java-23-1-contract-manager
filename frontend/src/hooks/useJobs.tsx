22c9a13e71f731eefb24a6ca8268b2034fd37b4f
lenaruppert <l.zi@gmx.de>
2023-04-03 18:07:29 +0200

    add method to hook frontend

--- frontend/src/hooks/useJobs.tsx
+++ frontend/src/hooks/useJobs.tsx
@@ -26,5 +26,17 @@ export function useJobs() {
             .then(data => setJobs(prevState => [...prevState, data]))
     }
 
-    return {jobs, addJob, getAllJobs}
+    function deleteJobById(id: string | undefined) {
+        axios.delete("/api/jobs/" + id)
+            .then(response => response.data)
+            .then(data => {
+                setJobs(prevState => {
+                    const currentJobs = prevState.filter(job => job.id !== id)
+                    return currentJobs
+                })
+            })
+            .catch(console.error)
+    }
+
+    return {jobs, addJob, getAllJobs, deleteJobById}
 }
