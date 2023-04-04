936785c8a2c45b041df5e104ad115fc4e47a41a1
lenaruppert <l.zi@gmx.de>
2023-04-04 11:29:55 +0200

    add LocalDate to model and repo backend

--- backend/src/main/java/com/github/lenaruppert/backend/model/Job.java
+++ backend/src/main/java/com/github/lenaruppert/backend/model/Job.java
@@ -3,6 +3,8 @@ package com.github.lenaruppert.backend.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
+
 @Document
 public record Job(
         @Id String id,
@@ -12,5 +14,6 @@ public record Job(
         String houseNumber,
         String postalCode,
         String city,
+        LocalDate orderDate,
         String clientId) {
 }
--- backend/src/main/java/com/github/lenaruppert/backend/model/JobDTO.java
+++ backend/src/main/java/com/github/lenaruppert/backend/model/JobDTO.java
@@ -1,10 +1,13 @@
 package com.github.lenaruppert.backend.model;
 
+import java.time.LocalDate;
+
 public record JobDTO(
         String title,
         String description,
         String street,
         String houseNumber,
         String postalCode,
+        LocalDate orderDate,
         String city) {
 }
