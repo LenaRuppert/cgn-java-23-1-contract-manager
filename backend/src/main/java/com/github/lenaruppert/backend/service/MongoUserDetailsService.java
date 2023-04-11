8dd49029216208dc014cda1e0737c4dea829474d
lenaruppert <l.zi@gmx.de>
2023-04-11 17:33:52 +0200

    fix MongoUserDetailsService

--- backend/src/main/java/com/github/lenaruppert/backend/service/MongoUserDetailsService.java
+++ backend/src/main/java/com/github/lenaruppert/backend/service/MongoUserDetailsService.java
@@ -68,7 +68,7 @@ public class MongoUserDetailsService implements UserDetailsService {
                 idService.generateId(),
                 user.username(),
                 passwordEncoder.encode(user.password()),
-                "basic"
+                "BASIC"
         );
         MongoUser out = mongoUserRepository.save(newUser);
 
