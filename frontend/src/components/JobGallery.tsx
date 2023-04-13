f6d17b87d180e209e0e1ede89ae07f96b063f908
lenaruppert <l.zi@gmx.de>
2023-04-13 14:48:43 +0200

    create search function for Job Gallery

--- frontend/src/components/JobGallery.tsx
+++ frontend/src/components/JobGallery.tsx
@@ -2,6 +2,7 @@ import {Job} from "../model/Job";
 import JobCard from "./JobCard";
 import {Grid} from "@mui/material";
 import Layout from "./Layout";
+import {ChangeEvent, useState} from "react";
 
 type JobGalleryProps = {
     jobs: Job[]
@@ -9,7 +10,31 @@ type JobGalleryProps = {
 }
 
 export default function JobGallery(props: JobGalleryProps) {
-    const jobCards = props.jobs.map(job => {
+    const [searchQuery, setSearchQuery] = useState('')
+    const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value);
+    }
+
+    const sortedJobs = props.jobs.slice().sort((a, b) => {
+        if (a.orderDate && b.orderDate) {
+            return new Date(a.orderDate).getTime() - new Date(b.orderDate).getTime();
+        } else if (a.orderDate) {
+            return -1;
+        } else if (b.orderDate) {
+            return 1;
+        } else {
+            return 0;
+        }
+    })
+        .filter((job) =>
+            job.title.toLowerCase().includes(searchQuery.toLowerCase())
+            || job.description.toLowerCase().includes(searchQuery.toLowerCase())
+            || job.city.toLowerCase().includes(searchQuery.toLowerCase())
+            || job.street.toLowerCase().includes(searchQuery.toLowerCase())
+            || job.postalCode.includes(searchQuery)
+        );
+
+    const jobCards = sortedJobs.map(job => {
         return (
             <JobCard job={job} key={job.id} deleteJobById={props.deleteJobById}/>
         )
@@ -18,6 +43,9 @@ export default function JobGallery(props: JobGalleryProps) {
     return (
         <Layout>
             <Grid sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Grid container item xs={10} justifyContent='center' marginTop={5}>
+                    <input type='text' value={searchQuery} onChange={handleSearchChange} placeholder='Auftrag suchen'/>
+                </Grid>
                 <Grid container item xs={10} justifyContent='center' marginTop={5}>
                     {jobCards}
                 </Grid>
