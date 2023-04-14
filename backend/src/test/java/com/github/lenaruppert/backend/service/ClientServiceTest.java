3418beed826a26ca7fcf6f782f5b54f595b267e8
lenaruppert <l.zi@gmx.de>
2023-04-14 14:40:22 +0200

    add test to clientService

--- backend/src/test/java/com/github/lenaruppert/backend/service/ClientServiceTest.java
+++ backend/src/test/java/com/github/lenaruppert/backend/service/ClientServiceTest.java
@@ -20,6 +20,7 @@ class ClientServiceTest {
     Client clientOne;
     ClientDTO clientDto;
     Client updateClient;
+    Client clientWithJob;
 
     @BeforeEach
     public void setUp() {
@@ -29,6 +30,7 @@ class ClientServiceTest {
         clientOne = new Client("1", "nameOfClient", Collections.emptyList());
         clientDto = new ClientDTO("nameOfClient");
         updateClient = new Client(clientOne.id(), clientDto.name(), clientOne.jobId());
+        clientWithJob = new Client("1", "nameOfClient", List.of("jobId"));
     }
 
     @Test
@@ -120,7 +122,7 @@ class ClientServiceTest {
     }
 
     @Test
-    void whenDeleteClientByIdWithNotExistingId_thenTrowNoSuchElementException() {
+    void whenDeleteClientByIdWithNotExistingId_thenThrowNoSuchElementException() {
         String nonExistingId = "1";
         when(clientRepository.findById(nonExistingId)).thenReturn(Optional.empty());
 
@@ -128,5 +130,21 @@ class ClientServiceTest {
 
         verify(clientRepository).findById(nonExistingId);
         verify(clientRepository, never()).deleteById(nonExistingId);
+    }
+
+    @Test
+    void whenDeleteClientByIdWithOpenJobs_thenThrowIllegalStateException() {
+        //GIVEN
+        Optional<Client> clientOptional = Optional.of(clientWithJob);
+        when(clientRepository.findById(clientWithJob.id())).thenReturn(clientOptional);
+
+        //WHEN
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> {
+            clientService.deleteClientById(clientWithJob.id());
+        });
+
+        //THEN
+        assertEquals("Client has jobs associated with it and cannot be deleted.", exception.getMessage());
+        verify(clientRepository, never()).deleteById(clientWithJob.id());
     }
 }
